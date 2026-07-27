6ec43075a8602c48f7a00f3fc31b4b35cb98a106
Sachin <ysachin036y@gmail.com>
2026-07-27 10:35:08 +0530

    Longest Consecutive Sequence

---
+++ ARRAY/long_conse_seq.java
@@ -0,0 +1,46 @@
+package ARRAY;
+import java.util.*;
+public class long_conse_seq {
+    static int LCS(HashSet<Integer> set)
+    {
+        if(set.isEmpty())
+        {
+            return 0;
+        }
+        int l=1,c=1,x=0;
+        for(int i : set)
+        {
+            if(!set.contains(i-1))
+            {
+                c=1;
+                x=i;
+                while(set.contains(x+1))
+                {
+                    c++;
+                    x=x+1;
+                }
+            }
+            l=Math.max(l, c);
+        }
+        return l;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> set=new HashSet<>();
+        System.out.print("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int x[] = new int[n];
+        System.out.println("Enter the array elements below ");
+        for (int i = 0; i < x.length; i++) {
+            x[i] = sc.nextInt();
+            set.add(x[i]);
+        }
+        System.out.println("Set : "+set);
+        // for(int i : set)
+        // {
+        //     System.out.print(i+" ");
+        // }
+        System.out.println("Longest Consecutive Sequence : "+LCS(set));
+        sc.close();
+    }
+}
